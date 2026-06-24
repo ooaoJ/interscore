67e12a21f147cdec8513f04e23b3811f6e5b7c2e
João Pedro <jpbentosperandio@gmail.com>
2026-06-24 11:14:38 -0300

    feat(routes): criando novas rotas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SchoolController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::middleware('role:platform_admin')->group(function () {
+       Route::get('/schools', [SchoolController::class, 'index']);
+    });
 });
