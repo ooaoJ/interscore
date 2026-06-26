cb939b6c1791af876c478fb6931c3476d299baed
João Pedro <jpbentosperandio@gmail.com>
2026-06-26 13:38:47 -0300

    feat(routes): criando novas rotas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\SchoolController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\InterclassController;
+use App\Http\Controllers\Api\ClassroomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,18 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('role:platform_admin')->group(function () {
        Route::apiResource('schools', SchoolController::class);
        Route::apiResource('users', UserController::class);
+    });
+
+    Route::middleware('role:platform_admin,school_manager')->group(function() {
+        Route::apiResource('interclasses', InterclassController::class);
+
+        Route::get('/grades', [ClassroomController::class, 'grades']);
+
+        Route::get('/interclasses/{interclass}/classrooms', [ClassroomController::class, 'index']);
+        Route::post('/interclasses/{interclass}/classrooms', [ClassroomController::class, 'store']);
+
+        Route::get('/classrooms/{classroom}', [ClassroomController::class, 'show']);
+        Route::put('/classrooms/{classroom}', [ClassroomController::class, 'update']);
+        Route::delete('/classrooms/{classroom}', [ClassroomController::class, 'destroy']);
     });
 });
